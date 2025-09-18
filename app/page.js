d73e6963c073d1e7bbcc3eaae9b6ac823e05d024
seinwinhtutAU <u6611040@au.edu>
2025-09-18 17:09:23 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,17 +1,24 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Link from "next/link";
 
 export default function BoxBasic() {
   return (
     <main>
-      <Box component="section" className="border border-gray-800 m-5 text-center">
-        <h1 className="text-3xl text-violet-950">Stock Management v1.0</h1>
-        <ul>
-          <li><a href="/product">Products</a></li>
-          <li><a href="/category">Category</a></li>
-        </ul>
-        
+      <Box
+        component="section"
+        className="border border-gray-800 m-5 text-center p-5"
+      >
+        <h1 className="text-3xl text-violet-950 mb-4">Stock Management v1.0</h1>
+        <div className="flex justify-center gap-4">
+          <Link href="/product" passHref>
+            <Button variant="contained">Products</Button>
+          </Link>
+          <Link href="/category" passHref>
+            <Button variant="contained">Category</Button>
+          </Link>
+        </div>
       </Box>
     </main>
   );
